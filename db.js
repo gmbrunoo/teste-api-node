9ba633602b88140e538bc7957fc49304015a2679
Bruno Godinho <gm.brunoo@gmail.com>
2022-09-14 17:12:52 -0300

    pg

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Pool = require('Pg').Pool;
+const Pool = require('pg').Pool;
 const pool = new Pool({
     connectionString: "postgres://zuccgakvpdorzo:03d1557cd482976a8223255bb552b433c16af65f7e1d1ac862dbd40aa1e54f56@ec2-52-73-184-24.compute-1.amazonaws.com:5432/db0uoa1gtg1n6s",
     ssl: {
